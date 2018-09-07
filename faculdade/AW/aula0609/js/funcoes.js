bd9e138d8f7eedfac234e7014c0c0ffbc4d203ec
Matheus Gouveia <matheus_guitarplayer@hotmail.com>
2018-09-06 21:17:01 -0300

    aula

--- faculdade/AW/aula0609/js/funcoes.js
+++ faculdade/AW/aula0609/js/funcoes.js
@@ -1,21 +1,51 @@
 function calcular(){
-    n1 = document.getElementById("n1").value;
-    n2 = document.getElementById("n2").value;
-    operacao = document.getElementById("operacao").value;
+    var n1 = document.getElementById("n1").value;
+    var n2 = document.getElementById("n2").value;
+    var operacao = document.getElementById("operacao").value;
+    
+    if(n1 != "" && n2 != "" && operacao != ""){
 
-    switch(operacao){
-        case 'adicao':
-            resultado = parseInt(n1) + parseInt(n2);
-            break;
-        case 'subtracao':
-            resultado = n1 - n2;
-            break;
-        case 'multiplicacao':
-            resultado = n1 * n2;
-            break;
-        case 'divisao':
-            resultado = n1 / n2;
-            break;
-    }
+        switch(operacao){
+            case 'adicao':
+                resultado = parseInt(n1) + parseInt(n2);
+                break;
+            case 'subtracao':
+                resultado = n1 - n2;
+                break;
+            case 'multiplicacao':
+                resultado = n1 * n2;
+                break;
+            case 'divisao':
+                resultado = n1 / n2;
+                break;
+
+        }
+
     document.getElementById("resultado").innerHTML = resultado;
+    } 
+    else{
+        
+        var retornon1 = document.getElementById("retornon1");
+        var retornon2 = document.getElementById("retornon2");
+        var retornoop = document.getElementById("retornoop");
+
+        if(n1 == ""){
+            retornon1.innerHTML = "Preencha este campo";
+        }
+        else{
+            retornon1.innerHTML = "";
+        }
+        if(n2 == ""){
+            retornon2.innerHTML = "Preencha este campo";
+        }
+        else{
+            retornon2.innerHTML = "";
+        }
+        if(operacao == ""){
+            retornoop.innerHTML = "Preencha este campo";
+        }
+        else{
+            retornoop.innerHTML = "";
+        }
+    }
 }
