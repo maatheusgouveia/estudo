5cfbe6c3a93e55b75c396b002ae5093d469477a3
Matheus A. Gouveia <matheus_guitarplayer@hotmail.com>
2018-09-15 01:42:34 -0300

    Javascript Rocketseat

---
+++ javascript/rocketseat/ToDo.js
@@ -0,0 +1,48 @@
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+
+var Todos = JSON.parse(localStorage.getItem('list_todos')) || [];
+
+function renderTodos(){
+    listElement.innerHTML = "";
+
+    for(todo of Todos){
+        var todoElement = document.createElement("li");
+        var todoText = document.createTextNode(todo);
+
+        var linkElement = document.createElement("a");
+        linkElement.setAttribute('href', '#');
+        var pos = Todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+        var linkText = document.createTextNode('Excluir');
+
+        linkElement.appendChild(linkText);
+        todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+        listElement.appendChild(todoElement);
+    }
+}
+
+function addTodo(){
+    var todoText = inputElement.value;
+
+    Todos.push(todoText + " ");
+    inputElement.value = '';
+    renderTodos();
+    saveToStorage();
+}
+
+buttonElement.onclick = addTodo;
+
+function deleteTodo(pos){
+    Todos.splice(pos, 1);
+    renderTodos();
+    saveToStorage();
+}
+
+function saveToStorage(){
+    localStorage.setItem('list_todos', JSON.stringify(Todos));
+}
+
+renderTodos();
